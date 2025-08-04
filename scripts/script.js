2e4c003fe5f1e3002e47ef5c1ca7f7f79b87bde3
Mary Okello <okellomary2012@gmail.com>
2025-08-04 13:39:56 +0300

    updated week 05 files

--- scripts/script.js
+++ scripts/script.js
@@ -9,10 +9,13 @@ const products = [
 // Populate select options
 window.addEventListener('DOMContentLoaded', () => {
   const select = document.getElementById('product');
-  products.forEach(product => {
-    const option = document.createElement('option');
-    option.value = product.name;
-    option.textContent = product.name;
-    select.appendChild(option);
-  });
+  if (select) {
+    products.forEach(product => {
+      const option = document.createElement('option');
+      option.value = product.name;
+      option.textContent = product.name;
+      select.appendChild(option);
+    });
+  }
 });
+
